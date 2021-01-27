0567ba067777a93cb6b08cfa6c28d79ca1b148eb
Andrey-Ionel <a-ionel@ukr.net>
2021-01-27 23:15:14 +0200

    litle changes

--- task1.js
+++ task1.js
@@ -1,46 +1,75 @@
-// Берём результат домашку из ДЗ 8. Примитивный todolist (начало) и добавляем логику.
-// Пользователь может ввести что-то в инпут и нажать на кнопку, после этого в списке ниже должна отобразится строка с тем что было введено в инпуте.
-// После этого инпут очищается.
+// Берём результат домашку из ДЗ 9. Примитивный todolist (конец) и добавляем логику.
 
-function onAddButtonClick() {
+// Каждое дело в списке может быть в двух состояниях (нужно сделать - желтый фон и сделано - зеленый фон). По умолчанию, дело в список добавляется в статусе нужно сделать. При клике на него, состояние меняется на противоположное.
 
-    const addListButton = document.querySelector('.js-add-todo');
-    const inputForAddList = document.querySelector('.js-todo-name');
-    const locationForAddList = document.querySelector('.js-todo-list');
-    const emptyListMessage = document.querySelector('.js-hidden-text');
+// Добавить в каждую строку кнопку удалить, при клике на которую элемент удаляется из списка.
 
-    addListButton.addEventListener('click', createNewList);
-    inputForAddList.addEventListener('keypress', disableEnterKey);
+// Когда удалили все элементы показывается сообщения которое сообщает что список пустой (оно должно быть всегда когда в списке нет элементов).
 
-    function disableEnterKey(event) {
+// Для реализации обязательно попробуйте использовать делегирование и интересные подходы из статей.
 
-        if (event.keyCode === 13) {
-            event.preventDefault();
-        }
+const addListButton = document.querySelector('.js-add-todo');
+const inputForAddList = document.querySelector('.js-todo-name');
+const todoList = document.querySelector('.js-todo-list');
+const emptyListMessage = document.querySelector('.js-hidden-text');
+const formWithTodoElements = document.querySelector('.js-todo-form');
 
-    }
+addListButton.addEventListener('click', onCreateNewTodoList);
+todoList.addEventListener('click', onRemoveTodoList);
+todoList.addEventListener('click', onChangeColorTodoList);
+formWithTodoElements.addEventListener('keypress', onDisableEnterKey);
 
-    function getInputValue() {
-        const currentInputValue = inputForAddList.value;
-        return currentInputValue;
-    }
+function onDisableEnterKey(event) {
 
-    function createNewList() {
-
-        if (getInputValue() === '' || getInputValue() === ' ') {
-            alert('Your input is empty');
-        } else {
-            emptyListMessage.hidden = true;
-            locationForAddList.insertAdjacentHTML('beforeend', `<li class="list-group-item">${getInputValue()}</li>`);
-            return clearsInputValue();
-        }
-
-    }
-
-    function clearsInputValue() {
-        inputForAddList.value = '';
+    if (event.keyCode === 13) {
+        event.preventDefault();
     }
 
 }
 
-onAddButtonClick();
+function onCreateNewTodoList() {
+    const currentInputValue = inputForAddList.value;
+    const initialInputValue = (inputForAddList.value = '');
+
+    if (currentInputValue && currentInputValue.trim().length) {
+        todoList.insertAdjacentHTML('beforeend', `<li class="list-group-item list-group-item-warning my-1">${currentInputValue}<i class="bi bi-x-square mx-3"></i></li>`);
+        changeVisibilityEmptyListMessage();
+        return initialInputValue;
+    } else {
+        alert('Your input is empty');
+        return initialInputValue;
+    }
+
+}
+
+function onChangeColorTodoList(event) {
+    const todoListElementTarget = event.target.classList.contains('list-group-item');
+    const todoListElement = event.target.closest('li');
+
+    if (todoListElementTarget) {
+        todoListElement.classList.toggle('list-group-item-warning');
+        todoListElement.classList.toggle('list-group-item-success');
+    }
+
+}
+
+function onRemoveTodoList(event) {
+    const todoCloseButton = event.target.classList.contains('bi-x-square');
+    const todoListElement = event.target.closest('li');
+
+    if (todoCloseButton) {
+        todoListElement.remove();
+        changeVisibilityEmptyListMessage()
+    }
+
+}
+
+function changeVisibilityEmptyListMessage() {
+
+    if (todoList.children.length >= 1) {
+        emptyListMessage.hidden = true;
+    } else {
+        emptyListMessage.hidden = false;
+    }
+
+}
